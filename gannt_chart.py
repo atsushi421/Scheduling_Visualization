9c09a73d3cee7f56ec25aa73378c6ab4fb952b9c
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-04-19 04:06:26 +0900

    fix: same taskIDs are same color

--- gannt_chart.py
+++ gannt_chart.py
@@ -5,11 +5,9 @@ import os
 import pandas as pd
 
 from bokeh.plotting import figure, output_file, save
-from bokeh.models import ColumnDataSource, Range1d, NumeralTickFormatter
+from bokeh.models import ColumnDataSource, Range1d, NumeralTickFormatter, Legend, LegendItem
 from bokeh.models.tools import HoverTool
-from bokeh.palettes import d3
-
-colors = d3['Category20'][20]
+from bokeh.palettes import d3, grey
 
 
 def option_parser() -> str:
@@ -31,20 +29,45 @@ def option_parser() -> str:
     return args.source_file_path, args.dest_dir, args.highlight_deadline_miss
 
 
+def get_taskID(id_str: str) -> int:  # HACK: jobID を分けて入力してもらう方式の方が良い．
+    return int(id_str.split(',')[0])
+
+
+def get_color_dict(source_dict: dict, highlight_deadline_miss: bool) -> dict:
+    # get taskIDs
+    taskIDs = set()
+    for task in source_dict['taskSet']:
+        taskIDs.add(get_taskID(task['taskName']))  # HACK: jobID を分けて入力してもらう方式の方が良い．
+    taskIDs = sorted(list(taskIDs))
+
+    # create color dict
+    color_dict = {}
+    if(highlight_deadline_miss):
+        color_dict['deadlineMiss'] = 'red'
+        greys = grey(len(taskIDs) + 2)
+        for taskID in taskIDs:
+            color_dict[str(taskID)] = greys[taskID + 1]
+    else:
+        colors = d3['Category20'][20]
+        for taskID in taskIDs:
+            color_dict[str(taskID)] = colors[taskID % 19]
+
+    return color_dict
+
+
 def main(source_file_path, dest_dir, highlight_deadline_miss):
     # json -> df
     with open(source_file_path) as f:
         source_dict = json.load(f)
     source_df = pd.DataFrame(columns=['coreID', 'taskID', 'Start', 'End', 'Color'])
+    color_dict = get_color_dict(source_dict, highlight_deadline_miss)
+
     for i, task in enumerate(source_dict['taskSet']):
         # Select color
-        if(highlight_deadline_miss):
-            if(task['deadlineMiss']):
-                color = 'red'
-            else:
-                color = '#DCDCDC'
+        if(task['deadlineMiss']):
+            color = color_dict['deadlineMiss']
         else:
-            color = colors[random.randint(0, 19)]
+            color = color_dict[str(get_taskID(task['taskName']))]
         
         source_df.loc[i] = [int(task['coreID']),  # HACK: If type of coreID is not <int>
                             str(task['taskName']),
@@ -56,7 +79,7 @@ def main(source_file_path, dest_dir, highlight_deadline_miss):
 
     # plot
     yaxis_list = []
-    for yaxis in  source_df.index.get_level_values(0).to_list():
+    for yaxis in source_df.index.get_level_values(0).to_list():
         if(yaxis not in yaxis_list):
             yaxis_list.append(yaxis)
     yaxis_list.sort(reverse=True)
@@ -64,7 +87,7 @@ def main(source_file_path, dest_dir, highlight_deadline_miss):
 
     p = figure(width=800,height=400,
                y_range=yaxis_list,
-               x_range=Range1d(0, source_dict['makespan']),
+               x_range=Range1d(0, 20),
                active_scroll='wheel_zoom',
                output_backend='svg')
     p.xaxis[0].formatter = NumeralTickFormatter(format='0,0')
@@ -75,7 +98,7 @@ def main(source_file_path, dest_dir, highlight_deadline_miss):
 
     yaxis_i = len(yaxis_list) - 1
     for _, task_df in source_df.groupby(level=0):
-        source = ColumnDataSource(task_df.droplevel(0))
+        source = ColumnDataSource(task_df.droplevel(0).reset_index())
         p.quad(left='Start',
                right='End',
                bottom=yaxis_i+0.3,
